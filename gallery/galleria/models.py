04788d4817d96a44e7b9e619ccba9c6136fe9b22
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-25 19:48:39 +0300

    adds image model to the app models.py

--- gallery/galleria/models.py
+++ gallery/galleria/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = '^$')
+    image_name = models.CharField(max_length =30)
+    image_description = models.CharField(max_length =100)
+    # location = models.ForeignKey(Location)
+    # category = models.ForeignKey(Category)
