20293acc8daa953bce04b3b38a7ab1f64851eb98
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-25 21:39:57 +0300

    adds test for deleting an object

--- gallery/galleria/tests.py
+++ gallery/galleria/tests.py
@@ -6,15 +6,23 @@ class ImageTestClass(TestCase):
 
     # Set up method
     def setUp(self):
-        self.firstimg = Image( image_name = 'firstimg', image_description = "blaaaaaaaaaaah")
+        self.new_image = Image( image_name = 'firstimg', image_description = "blaaaaaaaaaaah")
 
     # Testing instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.firstimg,Image))
+        self.assertTrue(isinstance(self.new_image,Image))
 
     #Testing Save Method
-    def test_save_method(self):
-        self.firstimg.save_image()
+    def test_save_image(self):
+        self.new_image.save_image()
         images = Image.objects.all()
         self.assertTrue(len(images) > 0) 
+
+    def test_delete_image(self):
+        self.new_image.save_image()
+        test_image =  Image( image_name = 'secondimg', image_description = "blah")
+        test_image.save_image()
+
+        self.new_image.delete_image()# Deleting a contact object
+        self.assertEqual(len(images),1)    
     
