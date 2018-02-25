a92f03cd2e8b8e4fb16ef0b2f22b54e7d68ac15f
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-25 21:17:48 +0300

    adds test_instance to confirm the object is instantiated correctly

--- gallery/galleria/tests.py
+++ gallery/galleria/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.firstimg = Image( image_name = 'firstimg', image_description = "blaaaaaaaaaaah")
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.firstimg,Image))
+
+    # #Testing Save Method
+    # def test_save_method(self):
+    #     self.firstimg.save_image()
+    #     images = Image.objects.all()
+    #     self.assertTrue(len(images) > 0) 
+    
