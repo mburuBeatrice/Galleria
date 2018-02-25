bfec1249d33b3e3ef4b59d97b8f9b5e29be0360e
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-25 20:27:37 +0300

    adds category model to app models.py

--- gallery/galleria/models.py
+++ gallery/galleria/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 # Create your models here.
+ 
+
+class Location(models.Model):
+    location_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.location_name   
+
+class Category(models.Model):
+    category_title = models.CharField(max_length = 30)        
+
 class Image(models.Model):
     image = models.ImageField(upload_to = '^$')
     image_name = models.CharField(max_length =30)
     image_description = models.CharField(max_length =100)
-    # location = models.ForeignKey(Location)
-    # category = models.ForeignKey(Category)
+    image_location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.image_name
@@ -16,11 +28,5 @@ class Image(models.Model):
     #     imagey = Image.objects.get(image_description = 'xoxoxoxox')
     #     print('Image found')
     # except DoesNotExist:
-    #     print('Image not found.Sorry.')    
-
-class Location(models.Model):
-    location_name = models.CharField(max_length = 30)
-
-    def __str__(self):
-        return self.location_name      
+    #     print('Image not found.Sorry.')          
     
