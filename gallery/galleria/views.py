869da447424544ef6b717b5571a8b901b73d8e00
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-25 18:27:35 +0300

    add a template folder and the first template

--- gallery/galleria/views.py
+++ gallery/galleria/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def start(request):
-    return HttpResponse('Welcome to Ze Galleria')
+    return render(request,'start.html')
