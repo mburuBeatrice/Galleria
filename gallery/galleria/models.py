1d85d032632091c1388c5cb9990c56ddafbd10f6
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-25 20:16:43 +0300

    adds location model to app models.py

--- gallery/galleria/models.py
+++ gallery/galleria/models.py
@@ -7,3 +7,20 @@ class Image(models.Model):
     image_description = models.CharField(max_length =100)
     # location = models.ForeignKey(Location)
     # category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.image_name
+    # class Meta:
+    #     ordering = ['name']    
+    # try:
+    #     imagey = Image.objects.get(image_description = 'xoxoxoxox')
+    #     print('Image found')
+    # except DoesNotExist:
+    #     print('Image not found.Sorry.')    
+
+class Location(models.Model):
+    location_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.location_name      
+    
