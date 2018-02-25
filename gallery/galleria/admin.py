7618ca6475848998c5b02f908370c712fccade49
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-02-25 22:37:55 +0300

    creates superuser

--- gallery/galleria/admin.py
+++ gallery/galleria/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Image,Location,Category
 
 # Register your models here.
+# class ImageAdmin(admin.ModelAdmin):
+#     filter_horizontal = ('Category',)
+
 admin.site.register(Image)
 admin.site.register(Category)
 admin.site.register(Location)
